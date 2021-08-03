57924aeb991c524d0da1618b8ee107677aae7777
Yanick Assignon <assignon>
2021-08-03 13:47:21 +0200

    csv data added to DB and django admin

--- dpi/settings.py
+++ dpi/settings.py
@@ -27,7 +27,8 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "False"
 
-ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(',')
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS",
+                          "127.0.0.1,localhost").split(',')
 
 
 # Application definition
@@ -44,7 +45,7 @@ DJANGO_APPS = [
 THIRD_PARTY_APPS = []
 
 LOCAL_APPS = [
-    
+    'hotels'
 ]
 
 INSTALLED_APPS = DJANGO_APPS + LOCAL_APPS + THIRD_PARTY_APPS
--- hotels/models.py
+++ hotels/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Hotel(models.Model):
+    city = models.CharField(max_length=30)  # city acronym
+    region = models.CharField(max_length=40)  # city region
+    name = models.CharField(max_length=100)  # hotel name
+
+    def __str__(self):
+        return str(self.name)
+
+
+class City(models.Model):
+    acronym = models.CharField(max_length=40)
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return str(self.name)
